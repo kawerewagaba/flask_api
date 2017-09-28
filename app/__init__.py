9b5521732c0a09ff3c1f605ded84c309097cd4e0
CK Wagaba <ckwagaba@gmail.com>
2017-09-28 07:13:19 +0000

    correcting config

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config = True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
