d253c716c2c9a585942fe2a899550d995b762e97
CK Wagaba <ckwagaba@gmail.com>
2017-09-29 07:18:12 +0000

    API functionality. Passed 2 tests. Not bad so far

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 # local imports
 from config import app_config
 
+from flask import request, jsonify, abort
+
 # db variable initialization
 db = SQLAlchemy()
 
@@ -14,9 +16,39 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
     
-    # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    # Get access to the Bucketlist model
+    from app.models import Bucketlist
+    
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
 
     return app
