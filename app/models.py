26425ccf91ac44daa43c0b4790a1295c6d4330d7
CK Wagaba <ckwagaba@gmail.com>
2017-09-30 06:28:36 +0000

    created user model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,36 @@
 from app import db
+from flask_bcrypt import Bcrypt
+
+class User(db.Model):
+    """This class defines the users table """
+
+    __tablename__ = 'users'
+
+    # Define the columns of the users table, starting with the primary key
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    bucketlists = db.relationship(
+        'Bucketlist', order_by='Bucketlist.id', cascade="all, delete-orphan")
+
+    def __init__(self, email, password):
+        """Initialize the user with an email and a password."""
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(password).decode()
+
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """Save a user to the database.
+        This includes creating a new user and editing one.
+        """
+        db.session.add(self)
+        db.session.commit()
+
 
 class Bucketlist(db.Model):
     """
