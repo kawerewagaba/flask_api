84f498fb12bf4ddea54a642cd7fcf95bdb3acba6
CK Wagaba <ckwagaba@gmail.com>
2017-10-08 21:55:34 +0300

    corrected configs

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 # local imports
 from instance.config import app_config
 
-# Load the views
-from app import views
+app = FlaskAPI(__name__, instance_relative_config = True)
+app.config.from_object(app_config['development'])
+app.config.from_pyfile('config.py')
 
 # db variable initialization
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
-def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config = True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    db.init_app(app)
-
-    return app
+# Load the views
+from app import views
