a25ebd981650d82aad22145570060f9ae7f79ff7
CK Wagaba <ckwagaba@gmail.com>
2017-10-10 23:23:21 +0300

    making config available to Travis

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,12 @@ position of this import really matters
 db = SQLAlchemy()
 
 # local import
-from instance.config import app_config
+from config import app_config
 from app.models import *
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
