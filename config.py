2e68df7c77da0596c21f47c4c79b0509f320f36e
CK Wagaba <ckwagaba@gmail.com>
2017-11-10 15:44:05 +0300

    updated config for deployment

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = 'p9Bv<3Eid9%$i01'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucketlist'
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bucketlist'
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
@@ -23,6 +23,8 @@ class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+    # heroku database url
+    SQLALCHEMY_DATABASE_URI = 'postgres://unambjfcrmrwxo:359619804ae18263ba9b774a9b78f2884009e216d7bf85f2f22d886ceedae60f@ec2-54-163-255-181.compute-1.amazonaws.com:5432/da0o373vrokhjc'
 
 app_config = {
     'development': DevelopmentConfig,
